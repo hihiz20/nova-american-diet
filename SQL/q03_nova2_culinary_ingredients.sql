1595c0d95f7518fe6cdcea33729beef388d219af
Hizkia Permata <163571012+hihiz20@users.noreply.github.com>
2026-05-20 09:49:03 -0400

    Delete SQL/q03_nova2_culinary_ingredients.sql

--- SQL/q03_nova2_culinary_ingredients.sql
+++
@@ -1,8 +0,0 @@
--- Q3: What are the top 15 most popular NOVA 2 (Processed Culinary Ingredients) in the US?
-
-SELECT branded_food_category, COUNT(*) AS product_count
-FROM branded_food
-WHERE nova_category LIKE 'NOVA 2%' AND market_country = 'United States'
-GROUP BY branded_food_category
-ORDER BY product_count DESC
-LIMIT 15;
