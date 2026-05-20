07507cfc23c703b421814e0cf7d2cd7122c7a408
Hizkia Permata <163571012+hihiz20@users.noreply.github.com>
2026-05-20 09:49:10 -0400

    Delete SQL/q04_nova1_unprocessed.sql

--- SQL/q04_nova1_unprocessed.sql
+++
@@ -1,8 +0,0 @@
--- Q4: What are the top 15 most popular NOVA 1 (Unprocessed) food categories in the US?
-
-SELECT branded_food_category, COUNT(*) AS product_count
-FROM branded_food
-WHERE nova_category LIKE 'NOVA 1%' AND market_country = 'United States'
-GROUP BY branded_food_category
-ORDER BY product_count DESC
-LIMIT 15;
