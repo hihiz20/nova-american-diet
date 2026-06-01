5e8413dc677d03949878bbd9bbaffa9adc4917b6
Hizkia Permata <163571012+hihiz20@users.noreply.github.com>
2026-06-01 10:46:26 -0400

    Delete SQL/NOVA/Q2_nova3_processed.sql

--- SQL/NOVA/Q2_nova3_processed.sql
+++
@@ -1,8 +0,0 @@
--- Q2: What are the top 15 most popular NOVA 3 (Processed) food categories in the US?
-
-SELECT branded_food_category, COUNT(*) AS product_count
-FROM branded_food
-WHERE nova_category LIKE 'NOVA 3%' AND market_country = 'United States'
-GROUP BY branded_food_category
-ORDER BY product_count DESC
-LIMIT 15;
