0b6ad812b87d1efd0862aefe7ab496ba00d3db71
Hizkia Permata <163571012+hihiz20@users.noreply.github.com>
2026-06-01 10:50:49 -0400

    Delete SQL/NOVA/Q5_top_categories_us_market.sql

--- SQL/NOVA/Q5_top_categories_us_market.sql
+++
@@ -1,7 +0,0 @@
--- Q5: What are the top food categories that dominate the overall US market?
-
-SELECT branded_food_category, COUNT(*) AS product_count
-FROM branded_food
-WHERE market_country = 'United States'
-GROUP BY branded_food_category
-ORDER BY product_count DESC;
